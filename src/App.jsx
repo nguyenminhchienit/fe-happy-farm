3523b1681d0d1d194abaaebd7e9e5ada4c6dde0c
NguyenMinhChien <minhchien03112003@gmail.com>
2024-06-17 12:42:55 +0700

    feat: ui signIn Out Listing layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import "./responsive.css";
-import Header from "./components/header/header";
 import data from "./data";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/public/Home/Home";
-import Footer from "./components/footer/footer";
 import DetailsPage from "./pages/public/Details";
+import NotFound from "./pages/public/NotFound";
+import SignIn from "./pages/public/SignIn";
+import SignUp from "./pages/public/SignUp";
+import Dashboard from "./components/Admin/dashboard/Dashboard";
+import PublicLayout from "./pages/public/PublicLayout";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div style={{ height: "2000px" }}>
-        <Header data={data.productData} />
+    <>
+      <div>
         <Routes>
-          <Route
-            exact={true}
-            path="/"
-            element={<Home data={data.productData} />}
-          />
-          <Route
-            exact={true}
-            path="/product/:id"
-            element={<DetailsPage data={data.productData} />}
-          />
+          <Route path="/" element={<PublicLayout />}>
+            <Route
+              exact={true}
+              path=""
+              element={<Home data={data.productData} />}
+            />
+            <Route
+              exact={true}
+              path="/product/:id"
+              element={<DetailsPage data={data.productData} />}
+            />
+            <Route exact={true} path="/signIn" element={<SignIn />} />
+            <Route exact={true} path="/signUp" element={<SignUp />} />
+            <Route exact={true} path="*" element={<NotFound />} />
+          </Route>
+          <Route exact={true} path="/admin/dash" element={<Dashboard />} />
         </Routes>
-        <Footer />
       </div>
-    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Admin/dashboard/Dashboard.jsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+  return <div style={{ height: "80vh" }}>Dashboard</div>;
+};
+
+export default Dashboard;
